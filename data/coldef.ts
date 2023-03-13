6e65be1656d15f455261268e9a91a6efd4013268
baurelien-la <baurelien@liveaction.com>
2023-03-13 18:20:06 +0100

    example without error

--- data/coldef.ts
+++ data/coldef.ts
@@ -1,8 +1,4 @@
 export const GROUPDEF = [
-  {
-    "headerName": "Cat 1",
-    "groupId": "cat1",
-    "children": [
       { "headerName": "Col 1", "field": "cat1-col1" },
       { "headerName": "Col 2", "field": "cat1-col2" },
       { "headerName": "Col 3", "field": "cat1-col3" },
@@ -12,13 +8,7 @@ export const GROUPDEF = [
       { "headerName": "Col 7", "field": "cat1-col7" },
       { "headerName": "Col 8", "field": "cat1-col8" },
       { "headerName": "Col 9", "field": "cat1-col9" },
-      { "headerName": "Col 0", "field": "cat1-col0" }
-    ]
-  },
-  {
-    "headerName": "Cat 2",
-    "groupId": "cat2",
-    "children": [
+      { "headerName": "Col 0", "field": "cat1-col0" },
       { "headerName": "Col 1", "field": "cat2-col1" },
       { "headerName": "Col 2", "field": "cat2-col2" },
       { "headerName": "Col 3", "field": "cat2-col3" },
@@ -28,13 +18,7 @@ export const GROUPDEF = [
       { "headerName": "Col 7", "field": "cat2-col7" },
       { "headerName": "Col 8", "field": "cat2-col8" },
       { "headerName": "Col 9", "field": "cat2-col9" },
-      { "headerName": "Col 0", "field": "cat2-col0" }
-    ]
-  },
-  {
-    "headerName": "Cat 3",
-    "groupId": "cat3",
-    "children": [
+      { "headerName": "Col 0", "field": "cat2-col0" },
       { "headerName": "Col 1", "field": "cat3-col1" },
       { "headerName": "Col 2", "field": "cat3-col2" },
       { "headerName": "Col 3", "field": "cat3-col3" },
@@ -45,6 +29,4 @@ export const GROUPDEF = [
       { "headerName": "Col 8", "field": "cat3-col8" },
       { "headerName": "Col 9", "field": "cat3-col9" },
       { "headerName": "Col 0", "field": "cat3-col0" }
-    ]
-  }
 ];
